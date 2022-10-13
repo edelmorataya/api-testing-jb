0b775179fd5f2e91711414a5a0e98e2189e2a587
Edel Morataya <Edel.Morataya@192.168.1.11>
2022-10-13 11:42:15 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express= require('express');
+
+const  app = express ();
+
+const posts = require('./posts')
+
+app.get('/posts', (req, res) =>
+{
+    res.json(posts)
+});
+
+app.get('/', (req, res) =>
+{
+    res.send("api running")
+});
+
+app.listen(process.env.PORT || 3000, 
+   () => console.log("Server is login")
+
+);
---
+++ posts.js
@@ -0,0 +1,50 @@
+module.exports = [
+    {
+      "_id": "634847f746a80b3925e9271e",
+      "index": 0,
+      "guid": "65a7a92a-9a9a-4e55-a4bb-44061688f15b",
+      "isActive": true,
+      "balance": "$3,833.25",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Lynn Dejesus",
+      "gender": "male",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Johnston Ratliff"
+        },
+        {
+          "id": 1,
+          "name": "Hardy Leonard"
+        }
+      ],
+      "greeting": "Hello, Lynn Dejesus! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "634847f72c843f93ceed4599",
+      "index": 1,
+      "guid": "9cc360b9-3081-4183-b5a3-bf8057c55823",
+      "isActive": false,
+      "balance": "$2,250.08",
+      "picture": "http://placehold.it/32x32",
+      "age": 27,
+      "eyeColor": "blue",
+      "name": "Hatfield Shepard",
+      "gender": "male",
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sherrie Clark"
+        },
+        {
+          "id": 1,
+          "name": "Sherman Strickland"
+        }
+      ],
+      "greeting": "Hello, Hatfield Shepard! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ]
