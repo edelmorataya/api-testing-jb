7af21a5f1c8b84c7a21ed2b876854ad7bc6608bb
Edel Morataya <edelmorataya@gmail.com>
2022-10-13 19:25:23 -0600

    api

--- index.js
+++ index.js
@@ -49,6 +49,85 @@ app.post('/api/infos', (req, res) =>
 }
 );
 
+const excs = [];
+const saves = [];
+const publishes = [];
+const validates = [];
+const stops = [];
+
+app.get('/api/execute', (req, res) =>
+{
+    res.send(excs)
+});
+
+app.post('/api/execute', (req, res) =>
+{
+    const exc =  req.body
+    excs.push(exc);
+    res.send(exc);
+}
+);
+
+app.get('/api/save', (req, res) =>
+{
+    res.send(saves)
+});
+
+
+app.post('/api/save', (req, res) =>
+{
+    const sav =  req.body
+    saves.push(sav);
+    res.send(sav);
+}
+);
+
+
+app.get('/api/publish', (req, res) =>
+{
+    res.send(publishes)
+});
+
+
+app.post('/api/publish', (req, res) =>
+{
+    const publi =  req.body
+    publishes.push(publi);
+    res.send(publi);
+}
+);
+
+app.get('/api/validate', (req, res) =>
+{
+    res.send(validates)
+});
+
+
+app.post('/api/validate', (req, res) =>
+{
+    const vali =  req.body
+    validates.push(vali);
+    res.send(vali);
+}
+);
+
+app.get('/api/stop', (req, res) =>
+{
+    res.send(stops)
+});
+
+
+app.post('/api/stop', (req, res) =>
+{
+    const sto =  req.body
+    stops.push(sto);
+    res.send(sto);
+}
+);
+
+
+
+
 
 app.listen(process.env.PORT || 3000, 
    () => console.log("Server is login")
