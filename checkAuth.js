401720bc96235cfb615c01bb1616ed27ecb2668f
Edel Morataya <edelmorataya@gmail.com>
2022-10-13 21:55:18 -0600

    checkAut

--- checkAuth.js
+++ checkAuth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next
 
 ) => {
     try{
-    const decoded =jwt.verify(req.body.token, token );
+    //const decoded =jwt.verify(req.body.token, token );
     
         next();
    
