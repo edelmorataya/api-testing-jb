a6ec224720bbd849bf2c391b63497f1ae664d563
Edel Morataya <edelmorataya@gmail.com>
2022-10-13 21:35:37 -0600

    checkAut

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express= require('express');
 
 const  app = express ();
-const checkAuth = require('./checkAuth');
 
 app.use(express.json());
 
@@ -75,7 +74,7 @@ app.get('/api/execute', (req, res) =>
     res.send(excs)
 });
 
-app.post('/api/execute', checkAuth, (req, res) =>
+app.post('/api/execute', (req, res) =>
 {
     const exc =  req.body
     excs.push(exc);
